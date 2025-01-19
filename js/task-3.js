7cef0788f9357107be6fd58d457ddebc3b5b2fcb
Ann-teres <anna.t.e.r.e.777@gmail.com>
2025-01-19 20:54:39 +0200

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,10 @@
-const sortByDescendingFriendCount = users => users.toSorted((a, b) => b.friends.length - a.friends.length);
+const sortByDescendingFriendCount = (users) => {
+  return users.sort((a, b) => b.friends.length - a.friends.length);
+};
 
 
-
-sortByDescendingFriendCount([
+sconsole.log(
+  sortByDescendingFriendCount([
     {
       name: "Moore Hensley",
       friends: ["Sharron Pace"],
@@ -38,7 +40,42 @@ sortByDescendingFriendCount([
       friends: ["Goldie Gentry", "Briana Decker"],
       gender: "female"
     }
-])
-  const sortedUsers = sortByDescendingFriendCount(users);
-
-console.log(sortedUsers);
+  ])
+);
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
