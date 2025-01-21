648a3f6fc3ecb561c90253ee0faabebc77330ce5
Ann-teres <anna.t.e.r.e.777@gmail.com>
2025-01-21 12:13:47 +0200

    add

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,7 @@ const sortByDescendingFriendCount = (users) => {
   return users.sort((a, b) => b.friends.length - a.friends.length);
 };
 
-
-sconsole.log(
+console.log(
   sortByDescendingFriendCount([
     {
       name: "Moore Hensley",
@@ -42,40 +41,3 @@ sconsole.log(
     }
   ])
 );
-// [
-//   {
-//     name: "Ross Vazquez",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Carey Barr",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Moore Hensley",
-//     friends: ["Sharron Pace"],
-//     gender: "male"
-//   }
-// ]
